465b9de7ae7e3eaf77d0cbc17f1d98091bd7a036
Iustin Nita <iustin.nita@interactions.ro>
2017-10-01 17:32:19 +0200

    work on homepage

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,57 @@
 $(document).ready(function(){
-      $('.carousel').slick({
-        infinite: true,
-        dots: true
-      });
-    });
+	$('header.carousel').slick({
+		infinite: true,
+		dots: true,
+		speed: 500,
+	    fade: true,
+	    cssEase: 'linear'
+	});
+	$('#slick-slide-control00').text('SEO');
+	$('#slick-slide-control01').text('PPC');
+	$('#slick-slide-control02').text('CRO');
+
+	$('.btn-next-service').on('click', function(event) {
+		event.preventDefault();
+		$('header.carousel .slick-next').click();
+	});
+
+	$('.case-studies-carousel').slick({
+		infinite: true,
+		dots: true
+	});
+
+	$('.trusted-by-carousel').slick({
+		infinite: true,
+		slidesToShow: 6,
+		slidesToScroll: 3,
+		responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 4,
+		        slidesToScroll: 3,
+		        infinite: true
+		      }
+		    },
+		    {
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 2
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2
+		      }
+		    }
+		    // You can unslick at a given breakpoint now by adding:
+		    // settings: "unslick"
+		    // instead of a settings object
+		]
+	})
+
+	$('.trusted-by-carousel img').css('filter', 'grayscale(100%)');
+});
