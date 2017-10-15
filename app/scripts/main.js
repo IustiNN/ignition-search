4e1dd60586d956217408a6e7f1137a47d8a0b53f
iustin <iustin.nitza@gmail.com>
2017-10-15 23:48:36 +0300

    add a lot of content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -57,12 +57,51 @@ $(document).ready(function(){
 		$('header.carousel .slick-next').click();
 	});
 
-	$('.case-studies-carousel').slick({
+  $('.case-studies-carousel').slick({
+    infinite: true,
+    dots: true
+  });
+
+	$('.testimonials-carousel').slick({
 		infinite: true,
 		dots: true
 	});
 
-	$('.trusted-by-carousel').slick({
+  $('.trusted-by-carousel').slick({
+    slidesToShow: 6,
+    slidesToScroll: 3,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 3
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+  })
+
+  $('.trusted-by-carousel img').css('filter', 'grayscale(100%)');
+
+
+	$('.trusted-by-carousel-about').slick({
 		slidesToShow: 6,
 		slidesToScroll: 3,
 		responsive: [
@@ -93,7 +132,7 @@ $(document).ready(function(){
 		]
 	})
 
-	$('.trusted-by-carousel img').css('filter', 'grayscale(100%)');
+	$('.trusted-by-carousel-about img').css('filter', 'grayscale(100%)');
 
 
 
